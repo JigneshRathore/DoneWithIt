1e01fc0a253aea36398d9c0e2fcb1e9f5ea61d64
Jignesh <jignesh@risurgetech.in>
2022-06-18 14:00:59 +0530

    Flexbox- flexBasis, flexGrow and flexShrink

--- FlexBox.js
+++ FlexBox.js
@@ -10,13 +10,22 @@ export default function FlexBox() {
         justifyContent: "center", //* main
         alignItems: "center", //* secondary
         alignContent: "center",
-        flexWrap: "wrap"
+        // flexWrap: "wrap"
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
+          // flexBasis: 100, //* width or height
+          // flex: 1,
+          
+          //* flexShrink
+          // flexShrink: 1,
+          width: 300,
+          //* or flex -1
+          flex: -1,
+
+          // width: 100,
           height: 100,
         }}
       />
@@ -30,20 +39,6 @@ export default function FlexBox() {
       <View
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
