64c9bdff8cd2a52a51322d110ecd66ff550e7183
Jignesh <jignesh@risurgetech.in>
2022-06-19 20:21:08 +0530

    View imagescreen Improvenemnts

--- App.js
+++ App.js
@@ -3,36 +3,38 @@ import GlobalStyles from "./GlobalStyles";
 // import DetactingOrientation from "./DetactingOrientation";
 // import FlexBox from "./FlexBox";
 // import AbsoluteandRelativePositioning from "./AbsoluteandRelativePositioning";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-// import ViewImageScreen from "./app/screens/ViewImageScreen";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 // import Borders from "./app/ui/Borders";
 // import Shadows from "./app/ui/Shadows";
 // import AppText from "./app/components/AppText";
 // import Icons from "./app/ui/Icons";
 // import AppButton from "./app/components/AppButton";
 // import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+// import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return (
-    <View
-      style={
-        {
-          // backgroundColor: "#f8f4f4",
-          // padding: 20,
-          // paddingTop: 20,
-          // alignItems: "center",
-        }
-      }
-    >
-      <ListingDetailsScreen
-        title="test"
-        subTitle="$200"
-        image={require("./app/assets/images/jacket.jpg")}
-      />
-    </View>
+    // <View
+    //   style={
+    //     {
+    //       // backgroundColor: "#f8f4f4",
+    //       // padding: 20,
+    //       // paddingTop: 20,
+    //       // alignItems: "center",
+    //     }
+    //   }
+    // >
+      
+      
+    //   <ListingDetailsScreen
+    //     title="test"
+    //     subTitle="$200"
+    //     image={require("./app/assets/images/jacket.jpg")}
+    //   />
+    // </View>
+    <ViewImageScreen />
     // <SafeAreaView style={GlobalStyles.androidSafeArea}>
-
     //   {/* <WelcomeScreen /> */}
     //   {/* <View
     //     style={{
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { StyleSheet, View, Image } from "react-native";
 import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function ViewImageScreen(props) {
   const chairImage = require("../assets/chair.jpeg");
   return (
-   <View style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color={colors.white} size={35} />
       </View>
       <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color={colors.white}
+          size={35}
+        />
       </View>
-     <Image style={styles.image} source={chairImage} resizeMode={"contain"}  />
-   </View>
-  )
+      <Image style={styles.image} source={chairImage} resizeMode={"center"} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.black,
-    flex: 1
+    flex: 1,
   },
   image: {
     height: "100%",
@@ -27,19 +34,19 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: colors.primary,
+    // backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
-    left: 30
+    left: 30,
   },
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: colors.secondary,
+    // backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
-    right: 30
-  }
+    right: 30,
+  },
 });
 
 export default ViewImageScreen;
